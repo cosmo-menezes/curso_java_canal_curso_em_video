35e3b093a1d25a850d3e58f5617a53817837b591
Cosmo Menezes de Souza <65937470+cosmo-menezes@users.noreply.github.com>
2020-06-30 10:12:57 -0300

    Exercicio da aula de Java
    Utilizamos a estrutura Switch de repetição

---
+++ ProgramaPernas/src/programapernas/ProgramaPernas.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programapernas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cosmo Menezes
+ */
+public class ProgramaPernas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner tec = new Scanner (System.in);
+        System.out.println("Quantas pernas?");
+        int perna = tec.nextInt();
+        System.out.print("Isso é um(a) ");
+        String tipo;
+        switch (perna){ 
+            case 1: 
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bipede";
+                break;
+            case 3:
+                tipo = "Tripe";
+                break;
+            case 4:
+                tipo = "Quadrupede";
+                break;
+            case 6:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "ET";
+                break;
+        }
+        System.out.println(tipo);
+        
+    }
+    
+}
